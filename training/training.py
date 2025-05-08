59235814f58127942dfa267afa8f8a397cad2753
Weronika Zygis <115404427+werka-z@users.noreply.github.com>
2025-05-08 18:57:47 +0200

    Track performance

--- training/training.py
+++ training/training.py
@@ -12,9 +12,10 @@ for epoch in range(num_epochs):
         label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
         
         # real data
-        output_real = netD(real_cpu).view(-1)
-        errD_real = criterion(output_real, torch.ones_like(output_real))
+        output = netD(real_cpu).view(-1)
+        errD_real = criterion(output, torch.ones_like(output))
         errD_real.backward()
+        D_x = output.mean().item()
         
         # fake data
         noise = torch.randn(b_size, nz, 1, 1, device=device)
@@ -23,19 +24,24 @@ for epoch in range(num_epochs):
         output_fake = netD(fake.detach()).view(-1)
         errD_fake = criterion(output_fake, torch.zeros_like(output_fake))
         errD_fake.backward()
+        D_G_z1 = output.mean().item()
 
         errD = errD_real + errD_fake
         optimizerD.step()
                 
         # update Generator
         netG.zero_grad()
+        label.fill_(real_label) # generator wants fake data labelled as real
         output = netD(fake).view(-1)
         errG = criterion(output, torch.ones_like(output))
         errG.backward()
+        D_G_z2 = output.mean().item()
         optimizerG.step()
 
         if i % 50 == 0:
-            print(f'[{epoch}/{num_epochs}][{i}/{len(dataloader)}]\tLoss_D: {errD.item():.4f}\tLoss_G: {errG.item():.4f}')
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                  % (epoch, num_epochs, i, len(dataloader),
+                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
         G_losses.append(errG.item())
         D_losses.append(errD.item())
