7526aeed629cad8cf8416c857ecdaaf4d7720bcd
vltkv <119106723+vltkv@users.noreply.github.com>
2025-05-09 19:23:48 +0200

    Add generator

--- models/generator.py
+++ models/generator.py
@@ -1,1 +1,32 @@
+class Generator(nn.Module):
+    def __init__(self, nz=100, ngf=64, nc=3):
+        super(Generator, self).__init__()
+        self.main = nn.Sequential(
+            # Input: Z latent vector (batch_size x nz x 1 x 1)
+            nn.ConvTranspose2d(nz, ngf * 8, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
 
+            # State: (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+
+            # State: (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+
+            # State: (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 2, ngf, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+
+            # State: (ngf) x 32 x 32
+            nn.ConvTranspose2d(ngf, nc, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.Tanh()  # output between [-1, 1]
+            # Output: (nc) x 64 x 64
+        )
+
+    def forward(self, input):
+        return self.main(input)
